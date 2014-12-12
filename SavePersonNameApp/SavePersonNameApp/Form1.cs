07a7f3d8d1bd7983a14ac54435e5b7b11901fa16
Shourav111 <ri.shourav777@gmail.com>
2014-12-12 16:03:52 +0600

    new file added

---
+++ SavePersonNameApp/SavePersonNameApp/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SavePersonNameApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ArrayList sr = new ArrayList();
+        private void nameSavebutton_Click(object sender, EventArgs e)
+        {
+            
+            if (sr.Count <= 10)
+            {
+                string personName = personNameTextbox.Text;
+                sr.Add(personName);
+                countLabel.Text = sr.Count.ToString();
+            }
+            if (sr.Count == 10)
+            {
+                foreach (var personNameAdd in sr)
+                {
+                    PersonNameListBox.Items.Add(personNameAdd);
+                }
+            }
+            
+        }
+
+        private void saveAllbutton_Click(object sender, EventArgs e)
+        {
+            foreach (var personNameAdd in sr)
+            {
+                PersonNameListBox.Items.Add(personNameAdd);
+            }
+        }
+    }
+}
